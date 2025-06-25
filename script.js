6eb8eb36735ec4bac761f12249f23c81eaac1501
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-06-25 20:30:54 +0300

    Add the ability to change the grid size

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const canvas = document.querySelector(".white-canvas")
-const defaultSize = 16;
+const canvas = document.querySelector(".white-canvas");
+const btn = document.querySelector(".changeSize");
+let defaultSize = 8;
 
 console.log(canvas);
 
 for(let i = 0; i < defaultSize*defaultSize; i++) {
     const pixel = document.createElement("div");
     pixel.className = "pixel";
+    pixel.style.flexBasis = 100 / defaultSize + "%";
+    pixel.style.aspectRatio ="1 / 1";
     canvas.appendChild(pixel);
+    
 }
 
 canvas.addEventListener('mouseout', (event) => {
@@ -15,3 +19,24 @@ canvas.addEventListener('mouseout', (event) => {
         target.style.backgroundColor = "black";
     }
 });
+
+btn.addEventListener("click", changeSizeCanvas)
+
+function changeSizeCanvas() {
+    canvas.innerHTML = "";
+
+    do {
+        size = prompt("Select the size of the canvas side(no more than 100): ");
+    } while (size > 100);
+    defaultSize = size;
+
+    for(let i = 0; i < defaultSize*defaultSize; i++) {
+        const pixel = document.createElement("div");
+        pixel.className = "pixel";
+        pixel.style.flexBasis = 100 / defaultSize + "%";
+        pixel.style.aspectRatio ="1 / 1";
+        canvas.appendChild(pixel);
+        
+    }
+}
+
